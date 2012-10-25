b7fd5b4bfeb5cb73ab647e1f15266dbd23080bfa
jm.pascal <jeanmarie.pascal@alfresco.com>
2012-10-25 22:15:02 +0200

    MOBILE-943 : Draft version of implementing direct open.

--- alfresco-mobile-android/src/org/alfresco/mobile/android/application/utils/UrlFinder.java
+++ alfresco-mobile-android/src/org/alfresco/mobile/android/application/utils/UrlFinder.java
@@ -10,34 +10,37 @@ import android.text.TextUtils;
 public class UrlFinder
 {
     private static final String NODEREF = "noderef=";
+
     private static final String ID = "id=";
 
     private static final List<String> patterns = new ArrayList<String>(2);
-    static {
+    static
+    {
         patterns.add(NODEREF);
         patterns.add(ID);
     }
 
-    public static String getIdentifier(String url){
+    public static String getIdentifier(String url)
+    {
         String identifier = null, tmp = null;
-        tmp = url.toLowerCase(); 
+        tmp = url.toLowerCase();
         for (String pattern : patterns)
         {
-            if (tmp.contains(pattern.toLowerCase())){
-                identifier = TextUtils.substring(tmp, tmp.lastIndexOf(pattern)+pattern.length(), tmp.length());
-                
-                if (identifier.contains("&"))
-                    identifier = TextUtils.substring(identifier, 0, identifier.indexOf("&"));
+            if (tmp.contains(pattern.toLowerCase()))
+            {
+                identifier = TextUtils.substring(tmp, tmp.lastIndexOf(pattern) + pattern.length(), tmp.length());
 
-                if (NodeRefUtils.isNodeRef(identifier))
-                    return identifier;
-                if (NodeRefUtils.isVersionIdentifier(identifier))
-                    return identifier;
-                if (NodeRefUtils.isIdentifier(identifier))
-                    return identifier;
+                if (identifier.contains("&"))
+                {
+                    identifier = TextUtils.substring(identifier, 0, identifier.indexOf("&"));
+                }
+
+                if (NodeRefUtils.isNodeRef(identifier)) { return identifier; }
+                if (NodeRefUtils.isVersionIdentifier(identifier)) { return identifier; }
+                if (NodeRefUtils.isIdentifier(identifier)) { return identifier; }
             }
         }
         return null;
     }
-    
+
 }
