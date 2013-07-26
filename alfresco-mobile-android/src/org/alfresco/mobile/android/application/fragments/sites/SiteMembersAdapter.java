37034080446de2d5896c96811b895e4ad1363182
Jean Marie Pascal <jeanmarie.pascal@alfresco.com>
2013-07-26 12:27:19 +0200

    First implementation of Person & Site Membership. MOBILE-1868 MOBILE-1869

---
+++ alfresco-mobile-android/src/org/alfresco/mobile/android/application/fragments/sites/SiteMembersAdapter.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (C) 2005-2013 Alfresco Software Limited.
+ * 
+ * This file is part of Alfresco Mobile for Android.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.alfresco.mobile.android.application.fragments.sites;
+
+import java.util.List;
+
+import org.alfresco.mobile.android.api.model.Person;
+import org.alfresco.mobile.android.application.ApplicationManager;
+import org.alfresco.mobile.android.application.R;
+import org.alfresco.mobile.android.application.manager.RenditionManager;
+import org.alfresco.mobile.android.application.utils.UIUtils;
+import org.alfresco.mobile.android.ui.fragments.BaseListAdapter;
+import org.alfresco.mobile.android.ui.utils.GenericViewHolder;
+
+import android.app.Fragment;
+import android.widget.LinearLayout;
+
+/**
+ * @since 1.3.0
+ * @author jpascal
+ *
+ */
+public class SiteMembersAdapter extends BaseListAdapter<Person, GenericViewHolder>
+{
+    private Fragment fragment;
+    
+    private List<Person> selectedItems;
+
+    private RenditionManager renditionManager;
+    
+    public SiteMembersAdapter(Fragment fr, int textViewResourceId, List<Person> listItems,
+            List<Person> selectedItems)
+    {
+        super(fr.getActivity(), textViewResourceId, listItems);
+        this.fragment = fr;
+        this.selectedItems = selectedItems;
+        this.renditionManager = ApplicationManager.getInstance(getContext()).getRenditionManager(fr.getActivity());
+
+    }
+
+    @Override
+    protected void updateBottomText(GenericViewHolder vh, Person item)
+    {
+        vh.bottomText.setText(item.getJobTitle());
+        if (selectedItems != null && selectedItems.contains(item))
+        {
+            UIUtils.setBackground(((LinearLayout) vh.icon.getParent().getParent()),
+                    getContext().getResources().getDrawable(R.drawable.list_longpressed_holo));
+        }
+        else
+        {
+            UIUtils.setBackground(((LinearLayout) vh.icon.getParent().getParent()), null);
+        }
+    }
+
+    @Override
+    protected void updateIcon(GenericViewHolder vh, Person item)
+    {
+        renditionManager.display(vh.icon, item.getIdentifier(), R.drawable.ic_avatar);
+    }
+
+    @Override
+    protected void updateTopText(GenericViewHolder vh, Person item)
+    {
+        vh.topText.setText(item.getFullName());
+    }
+
+}
