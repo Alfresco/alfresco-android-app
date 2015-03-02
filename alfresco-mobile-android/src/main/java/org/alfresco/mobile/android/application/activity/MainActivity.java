654fc813f38f1935211bd785750133161c1935f9
JM.PASCAL <jeanmarie.pascal@alfresco.com>
2015-03-02 20:34:26 +0100

    MOBILE-3223

--- alfresco-mobile-android/src/main/java/org/alfresco/mobile/android/application/activity/MainActivity.java
+++ alfresco-mobile-android/src/main/java/org/alfresco/mobile/android/application/activity/MainActivity.java
@@ -197,7 +197,7 @@ public class MainActivity extends BaseActivity
                     && !prefs.getBoolean(GeneralPreferences.HAS_ACCESSED_PAID_SERVICES, false))
             {
 
-                if (mdmManager.hasConfig())
+                if (!mdmManager.hasConfig())
                 {
                     // Check if we've prompted the user for Data Protection yet.
                     // This is needed on new AlfrescoAccount creation, as the
