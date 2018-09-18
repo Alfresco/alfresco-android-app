08d376cb026215b5319001a0da4fd9bc19e3dcea
Bogdan Roatis <aetheern@gmail.com>
2018-09-18 16:09:03 +0300

    ANDROID-663 Crash on Menu editor on attempt to save something or tap on Settings

--- alfresco-mobile-android/src/debug/java/org/alfresco/mobile/android/application/fragments/config/ConfigMenuEditorFragment.java
+++ alfresco-mobile-android/src/debug/java/org/alfresco/mobile/android/application/fragments/config/ConfigMenuEditorFragment.java
@@ -173,6 +173,7 @@ public class ConfigMenuEditorFragment extends AlfrescoFragment implements DevMen
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id)
             {
+                selectedPosition = position;
                 CreateConfigMenuItemFragment.with(getActivity())
                         .viewConfig((ViewConfigImpl) parent.getItemAtPosition(position)).display();
             }
